08772098744470b1d96a9736844e56b40a02c01b
Ubuntu <ubuntu@ip-172-31-25-172.ec2.internal>
2020-04-09 03:56:40 +0000

    First version of JavaDoc.

--- java/org/spark_vina/VinaDock.java
+++ java/org/spark_vina/VinaDock.java
@@ -5,10 +5,36 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.spark_vina.SparkVinaProtos.VinaResult;
 
+/**
+* <h2>Java API of the VinaDock C++ class</h2>
+* This class wraps the C++ VinaDock class through JNI.
+*
+* @author  Yunlong Liu
+* @version 0.1
+* @since   2020-04-08
+*/
 public final class VinaDock {
 
   private final long nativeHandle;
 
+  /**
+   * The constructor of the VinaDock class building a docking instance.
+   * The docking instance consists of
+   * <ul>
+   *   <li>receptor PDBQT
+   *   <li>A 3D box specifying the region to place the small molecule in.
+   * </ul>
+   * 
+   * @param centerX The X Coordinate of the center of the box
+   * @param centerY The Y Coordinate of the center of the box
+   * @param centerZ The X Coordinate of the center of the box
+   * @param sizeX The size on X dimension
+   * @param sizeY The size on Y dimension
+   * @param sizeZ The size on Z dimension
+   * @param cpu  The number of cpus used for this docking
+   * @param numModes The number of modes returned. 
+   * @return a VinaDock instance.
+   */
   public VinaDock(
       String receptorPath,
       double centerX,
@@ -26,6 +52,13 @@ public final class VinaDock {
     }
   }
 
+  /**
+   * This method docks a list of ligands with a specified filtering limit on
+   * the estimated binding constant.
+   * @param ligandStrs A list of ligand PDBQT strings.
+   * @param filterLimit A filtering limit. 
+   * @return List<VinaResult> A list of docking result.
+   */
   public List<VinaResult> vinaFit(List<String> ligandStrs, double filterLimit) {
     String[] ligandStringArray = new String[ligandStrs.size()];
     return nativeVinaFit(nativeHandle, ligandStringArray, filterLimit).stream()
@@ -40,6 +73,10 @@ public final class VinaDock {
         .collect(Collectors.toList());
   }
 
+  /**
+   * Call this method to finalize (destruct) a VinaDock instance. This method
+   * must be called explicitly when the instance is no longer used.
+   */
   public void finalize() {
     nativeDelete(nativeHandle);
   }
@@ -61,7 +98,15 @@ public final class VinaDock {
       long nativeHandle, String[] ligandStringArray, double filterLimit);
 
   private static void loadNativeLibrary() {
-    System.loadLibrary("");
+    String osName = System.getProperty("os.name").toLowerCase();
+    StringBuilder libraryPath = new StringBuilder("jni/libvina_jni");
+    // We only support Mac and Linux OS.
+    if (osName.contains("mac")) {
+      libraryPath.append(".dylib");
+    } else {
+      libraryPath.append(".so");
+    }
+    System.loadLibrary(libraryPath.toString());
   }
 
   static {
