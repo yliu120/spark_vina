c3b5d40e1c27b0993a05c5d9fc19b1d3d46df286
yunlongl <davislong198833@gmail.com>
2020-05-25 00:34:01 -0400

    Read .mol2 in and convert it to PDBQT

--- java/org/spark/tools/ZincUtils.java
+++ java/org/spark/tools/ZincUtils.java
@@ -2,6 +2,13 @@ package org.spark.tools;
 
 import com.google.common.base.Optional;
 import com.google.protobuf.InvalidProtocolBufferException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.spark_vina.CompoundProtos.Compound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +26,35 @@ public final class ZincUtils {
     } catch (InvalidProtocolBufferException e) {
       LOGGER.error("Unable to parse the returned PDBQT compound: {}", e.toString());
       return Optional.absent();
+    }
+  }
+
+  public static List<String> readAllMol2CompoundsFromFile(String path) {
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+      StringBuilder mol2StringBuilder = null;
+      List<String> mol2Strings = new ArrayList<>();
+      for (String line = bufferedReader.readLine(); line != null;
+          line = bufferedReader.readLine()) {
+        if (line.isEmpty()) {
+          continue;
+        }
+        if (line.contains("@<TRIPOS>MOLECULE")) {
+          if (mol2StringBuilder != null) {
+            mol2Strings.add(mol2StringBuilder.toString());
+          }
+          mol2StringBuilder = new StringBuilder(line);
+        } else {
+          mol2StringBuilder.append(line);
+        }
+        mol2StringBuilder.append('\n');
+      }
+      return mol2Strings;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return null;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
     }
   }
 
